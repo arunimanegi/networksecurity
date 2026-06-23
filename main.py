2d83a6b7d4efecea2a4fa33040d321b38a7039cf
arunimanegi <n.aarunima@gmail.com>
2026-06-23 19:43:24 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
-from networksecurity.entity.config_entity import TrainingPipelineConfig
 
+from networksecurity.entity.config_entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+    TrainingPipelineConfig
+)
 
 import sys
 
-if __name__ == '__main__':
-    try:
-        trainingpipelineconfig=TrainingPipelineConfig()
 
+if __name__ == "__main__":
+    try:
+        trainingpipelineconfig = TrainingPipelineConfig()
+
+        # Data Ingestion
         dataingestionconfig = DataIngestionConfig(
             trainingpipelineconfig
-            )        
-        data_ingestion=DataIngestion(dataingestionconfig)
+        )
+
+        data_ingestion = DataIngestion(dataingestionconfig)
+
         logging.info("Initiate the data ingestion")
-        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+
+        dataingestionartifact = (
+            data_ingestion.initiate_data_ingestion()
+        )
+
+        logging.info("Data Ingestion Complete")
         print(dataingestionartifact)
 
+        # Data Validation
+        data_validation_config = DataValidationConfig(
+            trainingpipelineconfig
+        )
+
+        data_validation = DataValidation(
+            dataingestionartifact,
+            data_validation_config
+        )
+
+        logging.info("Initiate the data validation")
+
+        data_validation_artifact = (
+            data_validation.initiate_data_validation()
+        )
+
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
     except Exception as e:
-        raise NetworkSecurityException(e,sys) 
+        raise NetworkSecurityException(e, sys)
